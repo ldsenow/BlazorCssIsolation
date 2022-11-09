59416675eeba0fa898ae5b722a208d32d7f39747
ldsenow <ldsenow@gmail.com>
2022-11-10 01:20:22 +1100

    Add project files.

---
+++ BlazorCssIsolation/Button.razor.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Components;
+using System.Text;
+
+namespace BlazorCssIsolation;
+
+public record ButtonDesignTokens : DesignTokens
+{
+    public string? ButtonPrimaryBorderColor { get; set; }
+}
+
+public partial class Button
+{
+    [CascadingParameter]
+    public Theme Theme { get; set; } = default!;
+
+    [Parameter]
+    public string Type { get; set; } = "primary";
+
+    [Parameter]
+    public string? Text { get; set; }
+}
---
+++ BlazorCssIsolation/ConfigProvider.razor.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Components;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BlazorCssIsolation;
+
+public partial class ConfigProvider
+{
+    [Parameter]
+    public Theme Theme { get; set; } = new();
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
+
+public class Theme
+{
+    public BaseDesignTokens Base { get; set; } = new();
+    public ButtonDesignTokens Button { get; set; } = new();
+}
+
+public abstract partial record DesignTokens
+{
+    private const string VAR_PREFIX = "--ant";
+
+    public string ToCssStyle()
+    {
+        var sb = new StringBuilder();
+
+        var props = GetType().GetProperties(
+            System.Reflection.BindingFlags.Instance |
+            System.Reflection.BindingFlags.Public);
+
+        foreach (var p in props)
+        {
+            var v = p.GetValue(this);
+            if (v != null)
+                sb.Append($"{VAR_PREFIX}-{PascalToKebabCase(p.Name)}: {v};");
+        }
+
+        return sb.ToString();
+    }
+
+    private static string PascalToKebabCase(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+            return name;
+
+        return KebabCaseRegex().Replace(name, "-$1").Trim().ToLower();
+    }
+
+    [GeneratedRegex("(?<!^)([A-Z][a-z]|(?<=[a-z])[A-Z0-9])", RegexOptions.Compiled)]
+    private static partial Regex KebabCaseRegex();
+}
+
+public record BaseDesignTokens : DesignTokens
+{
+    public string? Primary { get; set; }
+}
