67f0239abaf51b5f61c40e0f3f015a0e4a5025db
ldsenow <ldsenow@gmail.com>
2022-11-10 12:39:52 +1100

    Renamed func name

--- BlazorCssIsolation/DesignTokens.cs
+++ BlazorCssIsolation/DesignTokens.cs
@@ -19,19 +19,18 @@ public abstract record DesignTokens
         {
             var v = p.GetValue(this);
             if (v != null)
-                sb.Append($"{VAR_PREFIX}-{PascalToKebabCase(p.Name)}: {v};");
+                sb.Append($"{VAR_PREFIX}-{ToKebabCase(p.Name)}: {v};");
         }
 
         return sb.ToString();
     }
 
     private static readonly Regex KebabCaseRegex = new("(?<!^)([A-Z][a-z]|(?<=[a-z])[A-Z0-9])", RegexOptions.Compiled);
-    private static string PascalToKebabCase(string name)
+    private static string ToKebabCase(string name)
     {
         if (string.IsNullOrEmpty(name))
             return name;
 
         return KebabCaseRegex.Replace(name, "-$1").ToLower().Trim();
     }
-
 }
